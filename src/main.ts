9262bb0807dc98eb9a2321367821ac4312facf71
Luu Viet Anh <anhlv@gviet.vn>
2021-04-27 16:24:04 +0700

    update: change log format

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,30 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './module';
 import * as morgan from 'morgan';
 import * as config from 'config';
+import * as helmet from 'helmet';
 import { ExpressAdapter, NestExpressApplication } from '@nestjs/platform-express';
+import { INestApplication, Logger } from '@nestjs/common';
 const port = config.server.port;
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, new ExpressAdapter());
-  app.use(morgan('combined'));
+  // app.enable('trust proxy');
+  app.use(helmet());
+  // app.use(morgan('combined'));
+  app.disable('x-powered-by');
+  enableLogRequest(app);
   await app.listen(port, '0.0.0.0');
 }
+
+function enableLogRequest(app: INestApplication) {
+  const logger = new Logger('HTTPRequest');
+  const format = ':remote-addr - :remote-user ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" ":response-time ms"';
+
+  app.use(
+    morgan(format, {
+      stream: { write: (s) => logger.verbose(s) },
+      skip: (req) => req.url.includes('/health'),
+    }),
+  );
+}
+
 bootstrap();
